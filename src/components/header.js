f66f48d9387a1f39dd868f5110ef31e6e0b7aeb7
avinashmohare <mohareavinash80@gmail.com>
2023-11-08 22:05:34 +0530

    Final Changes Navbar Small Screen

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import classes from "../styles/Header.module.scss";
 import { Link } from "react-router-dom";
 
+import Twitter from "../assets/svgs/twitter";
+import Instagram from "../assets/svgs/instagram";
+import Fiverr from "../assets/svgs/fiverr";
+import Gmail from "../assets/svgs/gmail";
+
 const Header = () => {
     const [active, setActive] = useState(false);
     return (
@@ -43,6 +48,32 @@ const Header = () => {
                     <Link to="/contact">
                         <p className={classes.item}>Contact</p>
                     </Link>
+
+                    <div className={classes.socials}>
+                        <div className={classes.appContainer}>
+                            <div className={classes.app}>
+                                <div className={classes.appName}>
+                                    <Fiverr height="100%" width="100%" />
+                                </div>
+
+                                <div className={classes.appName}>
+                                    <Instagram height="100%" width="100%" />
+                                </div>
+
+                                <div className={classes.appName}>
+                                    <Gmail height="100%" width="100%" />
+                                </div>
+
+                                {/* <div className={classes.appName}>
+                                    <Links height="100%" width="100%" />
+                                </div> */}
+
+                                <div className={classes.appName}>
+                                    <Twitter height="100%" width="100%" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
